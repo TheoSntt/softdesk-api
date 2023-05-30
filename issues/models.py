4c7406367a22fd47dcc7aca8b5f5fa012564d076
Théo <sinatti.theo@gmail.com>
2023-05-30 11:52:21 +0200

    Add the choice options to the choice fields

--- issues/models.py
+++ issues/models.py
@@ -3,11 +3,27 @@ from django.conf import settings
 from projects.models import Project
 
 class Issue(models.Model):
+
+    class Priorite(models.TextChoices):
+        FAIBLE = 'Faible'
+        MOYENNE = 'Moyenne'
+        ÉLEVÉE = 'Élevée'
+    
+    class Balise(models.TextChoices):
+        BUG = 'Bug'
+        AMÉLIORATION = 'Amélioration'
+        TÂCHE = 'Tâche'
+
+    class Statut(models.TextChoices):
+        A_FAIRE = 'À faire'
+        EN_COURS = 'En cours'
+        TERMINÉ = 'Terminé'
+    
     title = models.fields.CharField(max_length=256)
     description = models.fields.CharField(max_length=2048)
-    tag = models.fields.CharField(max_length=128)
-    priority = models.fields.CharField(max_length=128)
-    status = models.fields.CharField(max_length=128)
+    tag = models.fields.CharField(choices=Balise.choices, max_length=128)
+    priority = models.fields.CharField(choices=Priorite.choices, max_length=128)
+    status = models.fields.CharField(choices=Statut.choices, max_length=128)
     project = models.ForeignKey(to=Project, on_delete=models.CASCADE, related_name='issues')
     author = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='issues_authored')
     assignee = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='issues_assigned')
--- projects/models.py
+++ projects/models.py
@@ -3,9 +3,16 @@ from django.conf import settings
 
 
 class Project(models.Model):
+
+    class Type(models.TextChoices):
+        BACK_END = 'back-end'
+        FRONT_END = 'front-end'
+        IOS = 'IOS'
+        ANDROID = 'Android'
+    
     title = models.fields.CharField(max_length=256)
     description = models.fields.CharField(max_length=2048)
-    type = models.fields.CharField(max_length=128)
+    type = models.fields.CharField(choices=Type.choices, max_length=128)
 
     contributors = models.ManyToManyField(
         settings.AUTH_USER_MODEL,
