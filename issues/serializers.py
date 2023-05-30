1324141d35cd3df3168cc20ba33882508712acf5
Théo <sinatti.theo@gmail.com>
2023-05-30 11:53:50 +0200

    Add control to issue creation : assignee must be a contributor (or author) to the project

--- issues/serializers.py
+++ issues/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from issues.models import Issue
 from projects.models import Project
+from authentication.models import User
 from contributors.serializers import UserSerializer
 
     
@@ -31,6 +32,20 @@ class IssueCreateSerializer(serializers.ModelSerializer):
         # Serializer's create method expected behaviour
         issue = Issue.objects.create(**validated_data)
         return issue
+
+    def validate_assignee(self, value):
+        print(value)
+        if value:
+            # Retrieve the assigned user
+            user = value
+            # Retrieve the Project
+            request = self.context.get('request')
+            project_id = request.parser_context['kwargs']['project_pk']
+            project = Project.objects.get(id=project_id)
+            # Checks whether the User is a contributor (or author) to the project
+            if project not in user.projects.all() and user != project.author:
+                raise serializers.ValidationError('That user do not contribute to that project.')
+        return value
 
 class IssueModifySerializer(serializers.ModelSerializer):
 
